53c8788216c26992b0243d1cb0e92df6c8030aa1
HEINTZ Benjamin <bheintz@emea.msad.sopra>
2016-10-17 17:46:25 +0200

    bot workflow

--- bot/RHBot.js
+++ bot/RHBot.js
@@ -9,54 +9,75 @@ export function rhbot(bot) {
 
     bot.dialog('/profile', [
         function (session) {
-            builder.Prompts.text(session, 'Bonjour ! Quel est votre nom ?');
+            session.send('Je dois d\'abord mieux vous connaître !');
+            builder.Prompts.text(session, 'Quel est votre nom ?');
         },
         function (session, results) {
             session.userData.name = results.response;
-            builder.Prompts.text(session, 'Ou habitez vous '+ session.userData.name + ' ?');
+            builder.Prompts.text(session, 'Ou habitez vous ' + session.userData.name + ' ?');
         },
         function (session, results) {
             session.userData.city = results.response;
-            builder.Prompts.confirm(session, "Voulez vous voir les offres a proximité de "+ session.userData.city +"?");
+            builder.Prompts.confirm(session, "Voulez vous voir les offres a proximité de " + session.userData.city + "?");
         },
         function (session, results) {
-            session.send('Je recherche des offres à proximité de %s !', session.userData.city);
+            if(results.response){
+                session.send('Je recherche des offres à proximité de %s !', session.userData.city);
+                session.send(getMdOffers(session.userData.city));
+            }
+            else{
+                session.send('Que puis je faire pour vous %s ?', session.userData.name);
+            }
             session.endDialog();
         }
     ]);
 
-    intents.matches(/^changer nom/i, [
+
+    intents.matches(/^changer profil/i, [
         function (session) {
             session.beginDialog('/profile');
-        },
-        function (session, results) {
-            session.send('Ok votre nom est %s !', session.userData.name);
         }
     ]);
 
-    intents.matches(/^offre/i, [
+    intents.matches(/^version/i, [
         function (session) {
-            if (!session.userData.city) {
-                session.beginDialog('/job');
-            }else{
-                session.beginDialog('/city');
-            }
-        },
-        function (session, results) {
-            session.send('Ok... Changed your name to %s', session.userData.name);
+            session.send('Ma version est 1.0 !');  
         }
     ]);
 
-    intents.onDefault([
+    intents.matches(/^offres/i, [
         function (session, args, next) {
-            if (!session.userData.name) {
+            if (!session.userData.city) {
+                session.send('Je dois d\'abord mieux vous connaître !');
                 session.beginDialog('/profile');
             } else {
                 next();
             }
         },
         function (session, results) {
-            session.send('Que puis je faire pour vous %s ?', session.userData.name);
+            session.send('Je recherche des offres à proximité de %s !', session.userData.city);
+            session.send(getMdOffers(session.userData.city));
         }
     ]);
+
+    intents.onDefault([
+        function (session, args, next) {
+            if (!session.userData.name) {
+                session.send('Bonjour je suis BOTRH !');
+                session.beginDialog('/profile');
+            } else {
+                session.send('Que puis je faire pour vous %s ?', session.userData.name);
+            }
+        }
+    ]);
+}
+
+function getMdOffers(ville){
+    return [
+        '### Offres à proximité de ' + ville,
+        "* Agent de maintenance (H/F)",
+        "* Mécanicien (H/F)",
+        "* Chef de gare (H/F)",
+        "* Agent de maintenance (H/F)"
+    ].join('\n\n');
 }
